dff5a7dbdee385362738e0f974aafd4307e21f05
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2018-04-04 22:13:47 +0200

    Add test for negatives

--- test_primes.py
+++ test_primes.py
@@ -17,6 +17,11 @@ class PrimesTestCase(unittest.TestCase):
         """Is zero correctly determined not to be prime?"""
         self.assertFalse(is_prime(0))
 
+    def test_negative_number(self):
+        """Is a negative number correctly determined not to be prime?"""
+        for index in range(-1, -10, -1):
+            self.assertFalse(is_prime(index))
+
 
 if __name__ == '__main__':
     unittest.main()
