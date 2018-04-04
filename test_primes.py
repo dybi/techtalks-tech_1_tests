7d03a9a3c2bcbe0d53206912f0f74c2410cb2999
Piotr Dybowski <piotr.p.dybowski@gmail.com>
2018-04-04 22:20:29 +0200

    Switch for loop to pytest parametrize

--- test_primes.py
+++ test_primes.py
@@ -1,27 +1,25 @@
-import unittest
+from assertpy import assert_that
+import pytest
+
 from primes import is_prime
 
 
-class PrimesTestCase(unittest.TestCase):
+class TestPrimes(object):
     """Tests for `primes.py`."""
 
     def test_is_five_prime(self):
         """Is five successfully determined to be prime?"""
-        self.assertTrue(is_prime(5))
+        assert_that(is_prime(5)).is_true()
 
     def test_is_four_non_prime(self):
         """Is four correctly determined not to be prime?"""
-        self.assertFalse(is_prime(4), msg='Four is not prime!')
+        assert_that(is_prime(4), 'Four is not prime!').is_false()
 
     def test_is_zero_not_prime(self):
         """Is zero correctly determined not to be prime?"""
-        self.assertFalse(is_prime(0))
+        assert_that(is_prime(0)).is_false()
 
-    def test_negative_number(self):
+    @pytest.mark.parametrize('negative', range(-1, -10, -1))
+    def test_negative_number(self, negative):
         """Is a negative number correctly determined not to be prime?"""
-        for index in range(-1, -10, -1):
-            self.assertFalse(is_prime(index))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert_that(is_prime(negative), "negatives could never be primes").is_false()
